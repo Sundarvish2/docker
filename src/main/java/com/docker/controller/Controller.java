dd316495180c10fe6b7c8e433374fb90cd128ec8
Sundarvish2 <mailtosundar21@gmail.com>
2024-01-05 16:18:31 +0530

    commit

---
+++ src/main/java/com/docker/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping(value="/hi")
+	public String print() {
+		return "HI";
+	}
+	
+}
